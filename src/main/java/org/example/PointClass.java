95a3324e90d63e1349b60a0c218be617988b9d54
Lia Araruna <liaararuna@gmail.com>
2023-12-20 15:42:34 +0000

    Criacao PointClass

---
+++ src/main/java/org/example/PointClass.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class PointClass {
+    private int coordX;
+    private int coordY;
+
+    public PointClass(int coordX, int coordY) {
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+
+    public int getCoordX() {
+        return coordX;
+    }
+    public void setCoordX(int coordX) {
+        this.coordX = coordX;
+    }
+    public int getCoordY() {
+        return coordY;
+    }
+    public void setCoordY(int coordY) {
+        this.coordY = coordY;
+    }
+}
