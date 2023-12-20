c3602ef10bae4707ff851793c972e9f53e9d7901
Lia Araruna <liaararuna@gmail.com>
2023-12-20 16:26:58 +0000

    Criacao classe rectangle

---
+++ src/main/java/org/example/Rectangle.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Rectangle {
+    private PointClass upperLeft;
+    private PointClass bottomRight;
+
+    public Rectangle(PointClass upperLeft, PointClass bottomRight) {
+        this.upperLeft = upperLeft;
+        this.bottomRight = bottomRight;
+    }
+}
